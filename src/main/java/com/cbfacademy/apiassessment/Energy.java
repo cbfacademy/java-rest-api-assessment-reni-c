8d35edd939e16f99852fca4c5278ae05471ac38e
Reni Ogun-Coker <reni.coker@hotmail.co.uk>
2023-11-02 21:44:48 +0000

    tried a different method for my test file and added data to EnergyData class

---
+++ src/main/java/com/cbfacademy/apiassessment/Energy.java
@@ -0,0 +1,5 @@
+package com.cbfacademy.apiassessment;
+
+public interface Energy {
+    HashMap<String, Integer> getAllElectricityProduction2022();
+}
--- src/main/java/com/cbfacademy/apiassessment/EnergyData.java
+++ src/main/java/com/cbfacademy/apiassessment/EnergyData.java
@@ -1,5 +1,31 @@
 package com.cbfacademy.apiassessment;
 
-public class EnergyData {
+import java.util.HashMap;
+
+public class EnergyData implements Energy {
     
+    /* this method should return/contain a list of countries and their respective 
+    electricity production levels (TWh) in 2022 */
+    private HashMap<String, Integer> electricityProduction2022;
+
+    public EnergyData() {
+        this.electricityProduction2022 = new HashMap<String, Integer>() {
+            {
+                put("China", 8833);
+                put("United States", 4510);
+                put("India", 1802);
+                put("Russia", 1165);
+                put("Japan", 1063);
+                put("Brazil", 668);
+                put("Canada", 657);
+                put("South Korea", 625);
+                put("Germany", 578);
+                put("France", 473);
+            }
+        };
+    }
+
+    public HashMap<String, Integer> getAllElectricityProduction2022() {
+        return this.electricityProduction2022;
+    }
 }
