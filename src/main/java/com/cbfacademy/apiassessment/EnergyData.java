9a15321b0de8201b961fb36934a8a6758152f294
Reni Ogun-Coker <reni.coker@hotmail.co.uk>
2023-11-05 16:10:20 +0000

    finalised approach and all tests have passed

--- src/main/java/com/cbfacademy/apiassessment/EnergyData.java
+++ src/main/java/com/cbfacademy/apiassessment/EnergyData.java
@@ -6,32 +6,12 @@ public class EnergyData {
 
     private HashMap<String, Integer> electricityProduction2022;
     private HashMap<String, Double> renewableElectricityProduction2022;
-    private String country;
-    private int production;
-    private double renewable;
-
-    
-    public EnergyData(String country, int production, double renewable) {
-        this.country = country;
-        this.production = production;
-        this.renewable = renewable;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public int getProduction() {
-        return production;
-    }
-
-    public double getRenewable() {
-        return renewable;
-    }
 
     /* this method should return/contain a list of countries and their respective 
-    electricity production levels (TWh) in 2022 */
-    public electricityProduction2022() {
+    electricity production levels (TWh) in 2022 */ //- maybe take this comment out
+    public EnergyData() {
+
+        // initialise electricity production data
         this.electricityProduction2022 = new HashMap<String, Integer>() {
             {
                 put("China", 8833);
@@ -46,15 +26,11 @@ public class EnergyData {
                 put("France", 473);
             }
         };
-    }
-
-    public HashMap<String, Integer> getElectricityProduction2022() {
-        return this.electricityProduction2022;
-    } 
 
     /* this method should return/contain a list of countries and the percentage of 
-    electricity produced from renewable sources in 2022 */
-    public renewableElectricityProduction2022() {
+    electricity produced from renewable sources in 2022 */ // maybe take this comment out
+        
+        // initialise renewable electricity production data
         this.renewableElectricityProduction2022 = new HashMap<String, Double>() {
             {
                 put("China", 30.6);
@@ -71,39 +47,11 @@ public class EnergyData {
         };
     }
 
+   public HashMap<String, Integer> getElectricityProduction2022() {
+        return electricityProduction2022;
+    } 
+
     public HashMap<String, Double> getRenewableElectricityProduction2022() {
         return this.renewableElectricityProduction2022;
     }
 }
-
-
-// import java.util.HashMap;
-    
-//     public class EnergyData {
-
-//     /* this method should return/contain a list of countries and their respective 
-//     electricity production levels (TWh) in 2022 */
-//     private HashMap<String, Integer> electricityProduction2022;
-
-//     public EnergyData() {
-//         this.electricityProduction2022 = new HashMap<String, Integer>() {
-//             {
-//                 put("China", 8833);
-//                 put("United States", 4510);
-//                 put("India", 1802);
-//                 put("Russia", 1165);
-//                 put("Japan", 1063);
-//                 put("Brazil", 668);
-//                 put("Canada", 657);
-//                 put("South Korea", 625);
-//                 put("Germany", 578);
-//                 put("France", 473);
-//             }
-//         };
-//     }
-
-    // public HashMap<String, Integer> getAllElectricityProduction2022() {
-    //     return this.electricityProduction2022;
-    // }
-// }
-
--- src/test/java/com/cbfacademy/apiassessment/EnergyDataTest.java
+++ src/test/java/com/cbfacademy/apiassessment/EnergyDataTest.java
@@ -1,104 +1,62 @@
 package com.cbfacademy.apiassessment;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-@DisplayName(value = "The Energy Data Class should")
+@DisplayName("The Energy Data Class should")
 public class EnergyDataTest {
 
-    // this test should return the name of a country
+    private EnergyData energyData;
+
+    @BeforeEach
+    public void setUp() {
+        energyData = new EnergyData();
+    }
+
+    // this test ensures the electricityProduction2022 HashMap has a list of countries
     @Test
-    @DisplayName("return the name of a country")
-    public void returnNameOfCountry() {
-        String country = new String();
-        final String actual = country;
-        assertThat(actual, is(notNullValue()));
+    @DisplayName("check electricityProduction2022 HashMap for a list of countries")
+    public void electrictyProduction2022HashMapHasListOfCountries() {
+        assertNotNull(energyData.getElectricityProduction2022().keySet());
     } 
 
-    // this test should return the electricity production of a country
+    // this test ensures the electricityProduction2022 HashMap has a list of production values
     @Test
-    @DisplayName("return electricity production of a country")
-    public void returnElectricityProductionOfACountry() {
-        Integer production = new Integer(0);
-        final Integer actual = production;
-        assertThat(actual, is(notNullValue()));
+    @DisplayName("check electricityProduction2022 HashMap for a list of electricity production values")
+    public void electricityProduction2022HashMapHasListOfElectricityProductionValues() {
+        assertNotNull(energyData.getElectricityProduction2022().values());
     }
 
-    // this test should return the percentage of electricity production of a country that derives from renewable sources
+    // this test should check that the number of countries contained in the electricityProduction2022 HashMap is 10
     @Test
-    @DisplayName("return the percentage of a country's electricity production that comes from renewable sources")
-    public void returnPercentageOfElectrictyProductionOfACountryThatComesFromRenewableSources() {
-        Double renewable = new Double(0);
-        final Double actual = renewable;
-        assertThat(actual, is(notNullValue()));
+    @DisplayName("check size of electricityProduction2022 HashMap")
+    public void returnsSizeOf2022ElectricityProductionHashMap() {
+        assertEquals(10, energyData.getElectricityProduction2022().size());
     }
+
+    // this test ensures the renewableElectricityProduction2022 HashMap has a list of countries
+    @Test
+    @DisplayName("check renewableElectricityProduction2022 HashMap for a list of countries")
+    public void renewableElectrictyProduction2022HashMapHasListOfCountries() {
+        assertNotNull(energyData.getRenewableElectricityProduction2022().keySet());
+    }
+
+    // this test ensures the renewableElectricityProduction2022 HashMap has a list of production values
+    @Test
+    @DisplayName("check renewableElectricityProduction2022 HashMap for a list of electricity production values")
+    public void renewableElectricityProduction2022HashMapHasListOfElectricityProductionValues() {
+        assertNotNull(energyData.getRenewableElectricityProduction2022().values());
+    }
+
+     // this test should check that the number of countries contained in the renewableElectricityProduction2022 HashMap is 10
+    @Test
+    @DisplayName("check size of renewableElectricityProduction2022 HashMap")
+    public void returnsSizeOf2022RenewableElectricityProductionHashMap() {
+        assertEquals(10, energyData.getRenewableElectricityProduction2022().size());
+    }
+
 }
-// import org.junit.jupiter.api.BeforeAll;
-// import org.junit.jupiter.api.DisplayName;
-// import org.junit.jupiter.api.Test;
-
-// @DisplayName(value = "The Energy Data Class should")
-// public class EnergyDataTest {
-
-//     @BeforeAll
-//     public void setUp() {
-//         energy = new EnergyData();
-//         electricityProduction2022 = energy.getAllElectricityProduction2022();
-//     }
-
-//     // this test should return global electricity production for 2022
-//     @Test
-//     @DisplayName("return electricity production for countries in 2022")
-//     public void returns2022ElectricityProductionPerCountry() {
-//         HashMap<String, Integer> actualResult = energy.getAllElectricityProduction2022();
-//         assertTrue(electricityProduction2022.size() == actualResult.size());
-//     }
-
-// }
-
-// import java.util.stream.Stream;
-
-// import org.junit.jupiter.api.DisplayName;
-// import org.junit.jupiter.params.ParameterizedTest;
-
-// @DisplayName(value = "The Energy Data class should")
-// public class EnergyDataTest {
-    
-//     /* use a parameterized test to return a list of countries and their respective energy 
-//     consumption each year so you're not having to repeat the code unnecessarily and the 
-//     use of '@MethodSource' gets the parameters from the method */
-//     @ParameterizedTest
-//     @DisplayName("should return energy consumption per country per year")
-//     @MethodSource("getAnnualEnergyConsumption")
-
-//     public void AnnualEnergyConsumption(String country, int energy, int year) {
-
-//         final string actual = EnergyData.isEnergyData(energy, year);
-//         assertThat(actual, is(country));
-//     }
-
-//     private static Stream<Arguments> getAnnualEnergyConsumption() {
-
-//         return Stream.of(
-//             arguments("China", 3507, 2020),
-//             arguments("United States", 2045, 2020),
-//             arguments("India", 872, 2020),
-//             arguments("Russia", 762, 2020),
-//             arguments("Japan", 387, 2020),
-//             arguments("China", 3689, 2021),
-//             arguments("United States", 2145, 2021),
-//             arguments("India", 936, 2021),
-//             arguments("Russia", 826, 2021),
-//             arguments("Japan", 404, 2021),
-//             arguments("China", 3801, 2022),
-//             argument("United States", 2182, 2022),
-//             arguments("India", 1005, 2022),
-//             arguments("Russia", 822, 2022),
-//             arguments("Japan", 400, 2022)
-//             );
-//     }
-// }
