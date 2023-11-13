991da349566598eccf9ed4c3856964a6835dcf71
Reni Ogun-Coker <reni.coker@hotmail.co.uk>
2023-11-13 20:46:00 +0000

    trying to tie everything together

---
+++ src/main/java/com/cbfacademy/apiassessment/EnergyAPI.java
@@ -0,0 +1,9 @@
+package com.cbfacademy.apiassessment;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// @RestController
+@RequestMapping("/energy")
+public class EnergyAPI {
+    
+}
--- src/main/java/com/cbfacademy/apiassessment/EnergyInfoApplication.java
+++ src/main/java/com/cbfacademy/apiassessment/EnergyInfoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+// @RestController
 public class EnergyInfoApplication {
     
 }
