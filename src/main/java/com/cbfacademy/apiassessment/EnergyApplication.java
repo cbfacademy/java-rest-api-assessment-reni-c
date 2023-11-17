dfc187176868bedf3569271471f457b2c960669d
Reni Ogun-Coker <reni.coker@hotmail.co.uk>
2023-11-17 19:13:24 +0000

    code seems ok but still can't get filepath to work

--- src/main/java/com/cbfacademy/apiassessment/EnergyApplication.java
+++ src/main/java/com/cbfacademy/apiassessment/EnergyApplication.java
@@ -9,31 +9,62 @@ import org.springframework.boot.SpringApplication;
 
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 @SpringBootApplication
 public class EnergyApplication {
 
-    private static final String filePath = "src\\main\\resources\\data\\newfile.json";
-    static EnergyService energyService = new EnergyService();
+    // private static final String filePath = "src\\main\\resources\\data\\newfile.json";
+    // static EnergyService energyService = new EnergyService();
 
     	public static void main(String[] args) {
 		SpringApplication.run(EnergyApplication.class, args);
+
+        // access the LinkedHashMap in EnergyService
+        EnergyService energyService = new EnergyService();
+        LinkedHashMap<Integer, LinkedHashMap<String, Double>> data = energyService.getRenewableElectricityProduction();
+
+        String filePath = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "data" + File.separator + "newfile.json";
+        saveToJSON(data, filePath);
+        LinkedHashMap<Integer, LinkedHashMap<String, Double>> readData = readFromJSON(filePath);
 	}
-    
-    // method to save to JSON file
-    static void saveToJSON() {
-        try (FileWriter writer = new FileWriter(filePath)) {
-          Gson gson = new Gson(); 
-          gson.toJson(energyService.getRenewableElectricityProduction(), writer); 
-          writer.flush();
+
+    //method to save to JSON file
+    private static void saveToJSON(LinkedHashMap<Integer, LinkedHashMap<String, Double>> data, String filePath) {
+        try { 
+            File file = new File(filePath);
+            if (!file.exists()) {
+                if (!file.getParentFile().mkdirs()) {
+                    throw new IOException("Failed to create directories.");
+                }
+                if (!file.createNewFile()) {
+                    throw new IOException("Failed to create file.");
+                }
+            }
+
+            FileWriter writer = new FileWriter(file);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(data, writer);
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    
+    // // method to save to JSON file
+    // static void saveToJSON() {
+    //     try (FileWriter writer = new FileWriter(filePath)) {
+    //       Gson gson = new Gson(); 
+    //       gson.toJson(energyService.getRenewableElectricityProduction(), writer); 
+    //       writer.flush();
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 
     // method to read from JSON file
-    static LinkedHashMap<Integer, LinkedHashMap<String, Double>> readFromJSON() {
+    static LinkedHashMap<Integer, LinkedHashMap<String, Double>> readFromJSON(String filePath) {
         try (FileReader reader = new FileReader(filePath)) {
             Gson gson = new Gson();
             Type type = new TypeToken<LinkedHashMap<Integer, LinkedHashMap<String, Double>>>() {}.getType();
--- src/main/java/com/cbfacademy/apiassessment/EnergyController.java
+++ src/main/java/com/cbfacademy/apiassessment/EnergyController.java
@@ -4,18 +4,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.util.LinkedHashMap;
 
 @RestController
 @RequestMapping("/energy")
 public class EnergyController {
 
-    // private final EnergyService energyService;
+    private final EnergyService energyService;
     
-    // public EnergyController(EnergyService energyService) {
-    //     this.energyService = energyService;
-    // }
+    public EnergyController(EnergyService energyService) {
+        this.energyService = energyService;
+    }
 
+    String filePath = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "data" + File.separator + "newfile.json";
     // private final EnergyApplication energyAEnergyApplication;
     // public EnergyController(EnergyApplication energyAEnergyApplication) {
     //     this.energyAEnergyApplication = energyAEnergyApplication;
@@ -24,7 +26,7 @@ public class EnergyController {
     // get the whole list of electricity production percentages from renewable sources from the JSON file
     @GetMapping("/allData")
     public LinkedHashMap<Integer, LinkedHashMap<String, Double>> getAllEnergyData() {
-        return EnergyApplication.readFromJSON();
+        return EnergyApplication.readFromJSON(filePath);
     }
 
     // get electricity production percentages for 2021
